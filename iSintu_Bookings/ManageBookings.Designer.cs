c22b1ef6949a332a8b929063d22fdb5f976cf734
Bongani Tabu <bonganitabu@gmail.com>
2023-08-31 17:02:56 +0200

    Manage Bookings Form Updated

---
+++ iSintu_Bookings/ManageBookings.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace iSintu_Bookings
+{
+    partial class ManageBookings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(120, 116);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(492, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(120, 302);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(263, 302);
+            button2.Name = "button2";
+            button2.Size = new Size(83, 35);
+            button2.TabIndex = 2;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(390, 302);
+            button3.Name = "button3";
+            button3.Size = new Size(91, 35);
+            button3.TabIndex = 3;
+            button3.Text = "View";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(622, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Search";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(692, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(515, 302);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(97, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // ManageBookings
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(840, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "ManageBookings";
+            Text = "Manage Bookings";
+            Load += ManageBookings_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+    }
+}
