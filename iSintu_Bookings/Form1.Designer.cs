fc44aba2ce3abac1ad0a408a46ae82a8c939b6f3
Bongani Tabu <bonganitabu@gmail.com>
2023-08-31 17:31:48 +0200

    Editing Login form GUI -Added top label as well

--- iSintu_Bookings/Form1.Designer.cs
+++ iSintu_Bookings/Form1.Designer.cs
@@ -33,28 +33,30 @@
             textBox2 = new TextBox();
             label1 = new Label();
             label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
             SuspendLayout();
             // 
             // btn_Login
             // 
-            btn_Login.Location = new Point(419, 323);
+            btn_Login.Location = new Point(557, 379);
             btn_Login.Name = "btn_Login";
-            btn_Login.Size = new Size(120, 36);
+            btn_Login.Size = new Size(102, 36);
             btn_Login.TabIndex = 0;
-            btn_Login.Text = "Login";
+            btn_Login.Text = "Clear";
             btn_Login.UseVisualStyleBackColor = true;
             btn_Login.Click += btn_Login_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(429, 176);
+            textBox1.Location = new Point(539, 232);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(429, 246);
+            textBox2.Location = new Point(539, 302);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 2;
@@ -62,7 +64,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(309, 176);
+            label1.Location = new Point(419, 232);
             label1.Name = "label1";
             label1.Size = new Size(60, 15);
             label1.TabIndex = 4;
@@ -71,17 +73,37 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(309, 246);
+            label2.Location = new Point(419, 302);
             label2.Name = "label2";
             label2.Size = new Size(57, 15);
             label2.TabIndex = 5;
             label2.Text = "Password";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(395, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 36);
+            button1.TabIndex = 6;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(462, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Sign in to Isintu Bookings ";
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1046, 672);
+            Controls.Add(label3);
+            Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox2);
@@ -100,5 +122,7 @@
         private TextBox textBox2;
         private Label label1;
         private Label label2;
+        private Button button1;
+        private Label label3;
     }
 }
